b7f7acdd046e83bd3a06e1c77b9040737789ab22
arashesmailian <arashesmailian@gmail.com>
2022-09-19 17:06:51 +0430

    users UI in home page initiated

---
+++ src/components/User.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+//components
+import Img from "../assets/image1.jpg";
+
+//styles
+import styles from "./user.module.css";
+
+const User = ({ user }) => {
+  return (
+    <div className={styles.user_wrapper}>
+      <div className={styles.user_info}>
+        <div className={styles.user_detail}>
+          <img
+            src={user.avatar || Img}
+            alt="avatar"
+            className={styles.avatar}
+          />
+          <h4>{user.name}</h4>
+        </div>
+        <div
+          className={
+            user.isOnline
+              ? styles.user_status_online
+              : styles.user_status_offline
+          }
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default User;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+//components
+import { db, auth } from "../firebase";
+import { collection, query, where, onSnapshot } from "firebase/firestore"; //onSnapShot is real time but getDoc executed once
+import User from "../components/User";
+
+//styles
+import styles from "./home.module.css";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  //states
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const usersRef = collection(db, "users"); // usersRef now refers to all users
+    const q = query(usersRef, where("uid", "not-in", [auth.currentUser.uid])); // getting all users except onlins's (such as me)
+    const unsub = onSnapshot(q, (querySnapshot) => {
+      //adding query to snapshot
+      let users = [];
+      querySnapshot.forEach((doc) => {
+        users.push(doc.data());
+      });
+      setUsers(users);
+    });
+    return () => unsub(); //cuase this method is realTime we need to unmount(????)
+  }, []);
+  console.log(users);
+
+  return (
+    <div className={styles.home_container}>
+      <div className={styles.users_container}>
+        {users.map((user) => (
+          <User key={user.uid} user={user} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Home;
