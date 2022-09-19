c549fd94a6fafaf61f052d6cab0d6521e10a1d59
arashesmailian <arashesmailian@gmail.com>
2022-09-19 08:59:50 +0430

    Navbar changed(different UI when user is loged in)+ logout button added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,43 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+//components
+import { auth, db } from "../firebase";
+import { signOut } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
+
 //styles
 import styles from "./navbar.module.css";
+import { async } from "@firebase/util";
 
 const Navbar = () => {
+  //functions
+  const handleSignout = async () => {
+    await updateDoc(doc(db, "users", auth.currentUser.uid), {
+      isOnline: false,
+    });
+    await signOut(auth);
+  };
+
   return (
     <nav>
       <h3>
         <Link to="/">Messenger</Link>
       </h3>
       <div>
-        <Link to="/register">Register</Link>
-        <Link to="/login">Login</Link>
+        {auth.currentUser ? (
+          <>
+            <Link to="/profile">Profile</Link>
+            <button className={styles.btn} onClick={handleSignout}>
+              Logout
+            </button>
+          </>
+        ) : (
+          <>
+            <Link to="/register">Register</Link>
+            <Link to="/login">Login</Link>
+          </>
+        )}
       </div>
     </nav>
   );
